63b1f873002d176edabd6ad02031efa953b55590
Andreas Ziegler <andreas.ziegler@fau.de>
2016-08-03 10:58:42 +0200

    Add heapify + testcase

--- python/min_heap.py
+++ python/min_heap.py
@@ -1,9 +1,11 @@
+import random
+
 # Implements a min-heap
 def siftUp(heap, index):
     while True:
         if index == 0:
             break
-        # Check if parent is bigger and swap
+        # Check if parent is bigger and possibly swap
         parent = (index-1)//2
         if heap[parent] > heap[index]:
             tmp = heap[parent]
@@ -35,22 +37,47 @@ def siftDown(heap, index):
         index = smallerC
 
 def insert(heap, val):
+    # Add new value at the end and restore heap property
     heap.append(val)
     siftUp(heap, len(heap)-1)
 
 def removeMin(heap):
     n = len(heap)
+    # Save minimum element for return
     retval = heap[0]
+    # Swap last element to front and delete it at the end
     heap[0] = heap[n-1]
     del heap[n-1]
+    # Restore heap property
     siftDown(heap, 0)
     return retval
 
+def heapify(heap):
+    for i in reversed(range((len(heap)-1)//2)):
+        siftDown(heap, i)
+
+# Test insertions
 heap = []
 for i in range(0, 11):
     insert(heap, i)
 
+# Test removals
 while heap:
     val = removeMin(heap)
     print "Removed " + str(val)
     print heap
+
+# Test heapify
+testheap = []
+for i in range(0, 100):
+    testheap.append(i)
+
+array = testheap[:]
+random.shuffle(testheap)
+heapify(testheap)
+
+result = []
+while testheap:
+    result += [removeMin(testheap)]
+
+assert(result == sorted(array))
